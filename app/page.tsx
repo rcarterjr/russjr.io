0c88a71e97ce691a8700aa821ba336153af7b112
rcarterjr <russell.c12@gmail.com>
2024-06-09 14:11:38 -0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,101 @@
 import Image from "next/image";
+import { myJobs, JobType } from "@/public/jobs";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className="flex">
+      <LeftSection />
+      <RightSection />
+    </div>
+  );
+}
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+function LeftSection() {
+  return (
+    <header className="z-10 p-24 w-full max-w-5xl items-center justify-between font-mono sticky self-start top-0">
+      <h1>russell carter</h1>
+      <h2>software engineer</h2>
+      <div className="flex mt-4">
+        <a
+          href="https://linkedin.com/in/russellcarterjr/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/linkedin-app-white-icon.svg"
+            height={20}
+            width={20}
+            alt="LinkedIn"
+            className="mr-6"
+          />
+        </a>
+        <a
+          href="mailto: russell.c12@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <p>russell.c12@gmail.com</p>
+        </a>
       </div>
+    </header>
+  );
+}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+function RightSection() {
+  return (
+    <main className="z-10 p-24 w-full max-w-5xl items-center justify-between font-mono">
+      <MyBio />
+      <ExperienceContainer />
+      <ProjectsContainer />
+    </main>
+  );
+}
+
+function MyBio() {
+  return (
+    <>
+      <h1 className="py-6">bio</h1>
+      <p className="m-0 max-w-[60ch] text-sm opacity-50">
+        Hello my name is Russell Carter. I am a software Engineer with 5 years
+        of experience. I have been a full stack developer for the last 4 years
+        of my career. I am proficient in TypeScript, JavaScript, React, Next.js,
+        Node.js, and many others.
+      </p>
+    </>
+  );
+}
+
+function ExperienceContainer() {
+  return (
+    <>
+      <h1 className="py-6">experience</h1>
+      {myJobs.map((job: JobType) => (
+        <Job {...job} key={job.company} />
+      ))}
+    </>
+  );
+}
+
+function Job({ company, title, description, years }: JobType) {
+  return (
+    <div className="flex py-4">
+      <div className="flex">
+        <p className="m-0 max-w-[60ch] text-sm opacity-50">{years}</p>
+      </div>
+      <div className="flex flex-col">
+        <h1>{company}</h1>
+        <h4>{title}</h4>
+        <p className="m-0 max-w-[60ch] text-sm opacity-50">{description}</p>
+      </div>
+    </div>
+  );
+}
+
+function ProjectsContainer() {
+  return (
+    <>
+      <h1 className="py-6">projects</h1>
+      <div className="mb-32 grid text-center">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -47,13 +103,14 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
+            project 1{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+          <p className="m-0 text-sm opacity-50">
+            This is the details of project 1 where I make something really cool
+            and you aare so impressed
           </p>
         </a>
 
@@ -64,13 +121,14 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
+            project 2{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p className="m-0 text-sm opacity-50">
+            This is the details of project 2 where I make something really cool
+            and you are so impressed
           </p>
         </a>
 
@@ -81,33 +139,17 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
+            project 3{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          <p className="m-0 text-sm opacity-50">
+            This is the details of project 3 where I make something really cool
+            and you are so impressed
           </p>
         </a>
       </div>
-    </main>
+    </>
   );
 }
---
+++ public/jobs.ts
@@ -0,0 +1,30 @@
+export type JobType = {
+  company: string;
+  years: string;
+  title: string;
+  description: string;
+};
+
+export const myJobs: JobType[] = [
+  {
+    company: "Fisker Inc.",
+    years: "2023 - 2024",
+    title: "Full Stack Software Engineer",
+    description:
+      "worked on the integrations team where I created several endpoints with Nest.js",
+  },
+  {
+    company: "Uhuru Network",
+    years: "2020 - 2022",
+    title: "Full Stack Software Engineer",
+    description:
+      "created several mobile and web apps with React and React Native, TypeScript, JavaScript, Node.js, MongoDB, and others.",
+  },
+  {
+    company: "ASRC Federal",
+    years: "2019 - 2020",
+    title: "Software Engineer",
+    description:
+      "worked on the integrations team where I created several endpoints with Nest.js",
+  },
+];
